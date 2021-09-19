416e3ae2bc6514a9b64156b74a996584bdd2d96d
m29mujib <mamujib29@gmail.com>
2021-09-19 15:24:41 +0700

    Contoh1

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Hallo Guysss</h1>";
+		echo"<br>Nama Saya                : Muhamad Abdul Mujib<br>";
+		echo"<br>Tempat Tanggal Lahir : Bogor, 29 Juni 2002 <br>";
+        echo"<br>Alamat               : Desa Cimande Kp.Tarikolot Rt 010/003 <br>";
+        echo"<br>Agama                : Islam <br>";
+
+	}
+}
